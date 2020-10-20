127083d289c11d38a04d4986ed08d20c0f019375
langlang.chen <langlang.chen@advantech.com.cn>
2020-10-20 10:19:08 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
         port: 9503,// 自定义端口
         proxy: {
             '/api': {
-                target: 'http://localhost:9091', // 你请求的第三方接口
+                target: '101.37.27.53:9091', // 你请求的第三方接口
                 changeOrigin: true, // 在本地会创建一个虚拟服务端，然后发送请求的数据，并同时接收请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
                 pathRewrite: {  // 路径重写，
                     '^/api': ''  // 替换target中的请求地址，也就是说以后你在请求http://api.douban.com/v2/XXXXX这个地址的时候直接写成/api即可。
